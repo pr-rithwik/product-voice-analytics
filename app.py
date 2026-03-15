88346c6e946dda846160c6a5ee542dbf3513cb1c
Rithwik <pr.rithwik.pro@gmail.com>
2026-03-15 16:14:46 +0530

    set app.py for prod

--- app.py
+++ app.py
@@ -39,5 +39,5 @@ demo = build_ui(
 )
 
 if __name__ == '__main__':
-    # demo.launch(server_name="0.0.0.0", server_port=7860)
-    demo.launch(share=True)
+    demo.launch(server_name="0.0.0.0", server_port=7860)
+    # demo.launch(share=True)
