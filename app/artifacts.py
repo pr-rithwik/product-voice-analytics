1beeb77dad5669059ba87337fd153887fd4c3c47
Rithwik <pr.rithwik.pro@gmail.com>
2026-03-15 16:04:39 +0530

    fix bug in download_artifacts function

--- app/artifacts.py
+++ app/artifacts.py
@@ -7,7 +7,7 @@ from constants import (
     HF_REPO_TYPE_MODEL, APP_CACHE_DIR, DATA_PROCESSED_DIR,
     TFIDF_VECTORIZER_PATH, LR_MODEL_PATH, DISTILBERT_DIR,
     PARQUET_PATH, TFIDF_VECTORIZER_FILE, LR_MODEL_FILE,
-    MODELS_DIR, DISTILBERT, PARQUET_FILE, 
+    MODELS_DIR, DISTILBERT, PARQUET_FILE, PRODUCT_LOOKUP_FILE,
     DISTILBERT_CONFIG_PATH, PRODUCT_LOOKUP_PATH
 )
 
@@ -23,11 +23,21 @@ def download_artifacts():
 
     # TF-IDF downloader
     if not os.path.exists(str(TFIDF_VECTORIZER_PATH)):
-        hf_hub_download(repo_id=HF_MODEL_REPO, filename=TFIDF_VECTORIZER_FILE, local_dir=str(MODELS_DIR), local_dir_use_symlinks=False)
+        hf_hub_download(
+            repo_id=HF_MODEL_REPO, 
+            filename=TFIDF_VECTORIZER_FILE, 
+            local_dir=str(MODELS_DIR), 
+            local_dir_use_symlinks=False
+        )
 
     # LR downloader
     if not os.path.exists(str(LR_MODEL_PATH)):
-        hf_hub_download(repo_id=HF_MODEL_REPO, filename=LR_MODEL_FILE, local_dir=str(MODELS_DIR), local_dir_use_symlinks=False)
+        hf_hub_download(
+            repo_id=HF_MODEL_REPO, 
+            filename=LR_MODEL_FILE, 
+            local_dir=str(MODELS_DIR), 
+            local_dir_use_symlinks=False
+        )
 
     # DISTILBERT downloader
     if not os.path.exists(DISTILBERT_CONFIG_PATH):
@@ -45,13 +55,19 @@ def download_artifacts():
 
     # Product Lookup downloader
     if not os.path.exists(str(PRODUCT_LOOKUP_PATH)):
-        hf_hub_download(repo_id=HF_DATASET_REPO, filename='product_lookup.csv', local_dir=str(MODELS_DIR), local_dir_use_symlinks=False)
+        hf_hub_download(
+            repo_id=HF_DATASET_REPO, 
+            filename=PRODUCT_LOOKUP_FILE, 
+            local_dir=str(DATA_PROCESSED_DIR),
+            repo_type=HF_REPO_TYPE_DATASET, 
+            local_dir_use_symlinks=False
+        )
 
     if not os.path.exists(str(PARQUET_PATH)):
         hf_hub_download(
             repo_id=HF_DATASET_REPO,
             filename=PARQUET_FILE,
-            local_dir=str(MODELS_DIR),
+            local_dir=str(DATA_PROCESSED_DIR),
             repo_type=HF_REPO_TYPE_DATASET,
             local_dir_use_symlinks=False
         )
